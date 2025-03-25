e091e12b850a33bfe3c74aa0ab43f89e0cef71f3
Tpj-root <trichy_hackerspace@outlook.com>
2025-03-25 19:13:47 +0530

    And Here We Go

--- alias_run.sh
+++ alias_run.sh
@@ -756,6 +756,45 @@ alias gitrestore='git restore -- .'
 alias gitundo='git reset'
 
 
+# Status of git rep
+# 
+#
+gitcheck() 
+{
+
+# Change to the main directory
+cd $HOME/Desktop/MY_GIT || exit
+
+# Flag to track if any changes are detected
+changes_detected=false
+
+# Loop through each folder in the directory
+for folder in */; do
+    # Navigate to the folder
+    cd "$folder" || continue
+
+    # Check git status and capture the output
+    status=$(git status --porcelain)
+
+    # If there are any changes, print the folder name in red
+    if [[ -n "$status" ]]; then
+        echo -e "Changes detected in folder: \033[31m$folder\033[0m"
+        changes_detected=true
+    fi
+
+    # Go back to the main directory
+    cd ..
+done
+
+# If no changes were detected, print the green message
+if ! $changes_detected; then
+    echo -e "\033[32mThere is no change where there is no action.\033[0m"
+fi
+
+}
+
+
+
 #
 # This will check if the key is added. If not,
 # it will prompt for a passphrase.
